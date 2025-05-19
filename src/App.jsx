d9ec69b64268cbd692d489bca9ea974036fe78b3
Carlomadella <madella871@gmail.com>
2025-05-20 00:10:31 +0200

    [UPD] creata e stampata a video l'homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState, useEffect } from 'react'
-import HomePage from './pages/HomePage';
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={About} />
+        <Route path='/products' Component={Products} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
